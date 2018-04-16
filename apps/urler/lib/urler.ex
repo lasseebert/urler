6a26e788f1b35de16cbce5cd5224f642f6142681
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2018-04-16 22:30:55 +0200

    Main interface for Urler

--- apps/urler/lib/urler.ex
+++ apps/urler/lib/urler.ex
@@ -1,18 +1,48 @@
 defmodule Urler do
   @moduledoc """
-  Documentation for Urler.
+  A URL shortener application
   """
+
+  alias Urler.Encoder
+  alias Urler.Store
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Urler.hello
-      :world
-
+  Shortens a URL and returns the path of the short_url.
   """
-  def hello do
-    :world
+  @spec shorten(String.t()) :: {:ok, String.t()}
+  def shorten(url) do
+    {:ok, id} = Store.put(url)
+
+    short_url =
+      id
+      |> Encoder.encode()
+      |> full_short_url()
+
+    {:ok, short_url}
   end
+
+  @doc """
+  Resolves a short URL to a URL
+  """
+  @spec resolve(String.t()) :: {:ok, String.t()} | :not_found | :invalid
+  def resolve(short_url) do
+    with {:ok, short_string} <- short_url_to_encoded_id(short_url),
+         {:ok, id} <- Encoder.decode(short_string),
+         {:ok, url} <- Store.fetch(id) do
+      {:ok, url}
+    else
+      :not_found -> :not_found
+      :error -> :invalid
+    end
+  end
+
+  defp full_short_url(short_string) do
+    "/" <> short_string
+  end
+
+  defp short_url_to_encoded_id("/" <> short_string) do
+    {:ok, short_string}
+  end
+
+  defp short_url_to_encoded_id(_), do: :error
 end
--- apps/urler/test/urler_test.exs
+++ apps/urler/test/urler_test.exs
@@ -1,8 +1,24 @@
 defmodule UrlerTest do
   use ExUnit.Case
-  doctest Urler
 
-  test "greets the world" do
-    assert Urler.hello() == :world
+  alias Urler.Store
+
+  test "shorten a URL and look it up again" do
+    Store.start_link()
+    url = "https://example.com/foo"
+
+    {:ok, short_url} = Urler.shorten(url)
+    {:ok, resolved_url} = Urler.resolve(short_url)
+
+    assert resolved_url == url
+    assert short_url =~ ~r{^/[a-z0-9]+$}
+  end
+
+  test "looking up short_url without correct prefix" do
+    assert Urler.resolve("abcd") == :invalid
+  end
+
+  test "looking up short_url with bogus short_string" do
+    assert Urler.resolve("/%%") == :invalid
   end
 end
