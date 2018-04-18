faaee8ccda065c2dead3b8f255223d3ee305fb40
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2018-04-18 09:20:19 +0200

    Added the two endpoints

--- apps/urler_web/lib/urler_web/server.ex
+++ apps/urler_web/lib/urler_web/server.ex
@@ -13,9 +13,48 @@ defmodule UrlerWeb.Server do
   end
 
   @impl Raxx.Server
-  def handle_request(%{method: :GET, path: []}, _state) do
-    response(:ok)
-    |> set_header("content-type", "text/plain")
-    |> set_body("Hello, World!")
+  def handle_request(%{method: :GET, path: [short_string]}, _state) do
+    path = "/" <> short_string
+
+    case Urler.resolve(path) do
+      {:ok, url} ->
+        301
+        |> response()
+        |> set_header("location", url)
+        |> set_body("")
+
+      :not_found ->
+        404
+        |> response()
+        |> set_header("content-type", "application/json")
+        |> set_body(Poison.encode!(%{message: "Not found"}))
+    end
+  end
+
+  def handle_request(%{method: :POST, path: ["urls"]} = request, _state) do
+    with body <- request.body,
+         {:ok, params} <- Poison.decode(body),
+         {:ok, url} <- Map.fetch(params, "url"),
+         {:ok, short_url} <- Urler.shorten(url) do
+      response_body = %{url: url, short: short_url} |> Poison.encode!()
+
+      201
+      |> response()
+      |> set_header("content-type", "application/json")
+      |> set_body(response_body)
+    else
+      _error ->
+        400
+        |> response()
+        |> set_header("content-type", "application/json")
+        |> set_body(Poison.encode!(%{message: "Bad request"}))
+    end
+  end
+
+  def handle_request(_catch_all, _state) do
+    404
+    |> response()
+    |> set_header("content-type", "application/json")
+    |> set_body(Poison.encode!(%{message: "Not found"}))
   end
 end
